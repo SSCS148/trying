5246bb3457b07beb488e42e120076777461f08b6
SSCS148 <160610047+SSCS148@users.noreply.github.com>
2024-06-17 15:09:14 +0300

    refresh

--- server/routes/comment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comment');
-
-router.post('/', commentController.createComment);
-router.get('/', commentController.getAllComments);
-router.post('/like', commentController.likeComment);
-
-module.exports = router;
